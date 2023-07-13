f79babe03e46ce5884ac037069cd2fefd43fc4df
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2023-07-13 15:10:16 +0300

    Initial Commit

---
+++ CoinPricesApp-SwiftUI/CoinPricesApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinPricesApp_SwiftUIApp.swift
+//  CoinPricesApp-SwiftUI
+//
+//  Created by Gorkem on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoinPricesApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
