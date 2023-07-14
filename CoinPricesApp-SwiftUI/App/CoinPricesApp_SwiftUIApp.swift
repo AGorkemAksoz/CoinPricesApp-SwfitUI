fd351bed56cc55df37dcca294a4ab4a9089a6bee
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2023-07-14 11:28:45 +0300

    All coins section has done

--- CoinPricesApp-SwiftUI/App/CoinPricesApp_SwiftUIApp.swift
+++ CoinPricesApp-SwiftUI/App/CoinPricesApp_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CoinPricesApp_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            HomeView(coin: dummyCoin)
         }
     }
 }
